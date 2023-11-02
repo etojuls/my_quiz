ec4fafa5d66a719d1acae46bdf2431abcba97239
uliagalkina <ygalkina@southmedia.ru>
2023-11-02 12:01:28 +0300

    config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/vue_to_do/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/my_quiz/" : "/",
 });
